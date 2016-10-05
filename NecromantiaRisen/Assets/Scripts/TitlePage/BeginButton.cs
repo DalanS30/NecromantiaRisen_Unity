95361dc34e92c892016f3284ea94807e1ba15110
DalanS30 <daveyas30@live.com>
2016-10-04 22:05:06 -0500

    Menu and Working Scroll

---
+++ NecromantiaRisen/Assets/Scripts/TitlePage/BeginButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BeginButton : MonoBehaviour {
+
+	public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
